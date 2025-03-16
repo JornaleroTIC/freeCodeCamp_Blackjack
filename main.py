7c7bcc0efc0a5196848499f969df3566ff450765
JornaleroTIC <jornalerotic@gmail.com>
2025-03-16 19:08:01 +0100

    Added Deck and Card classes.

--- main.py
+++ main.py
@@ -1,40 +1,51 @@
 # Libraries
 import random
 
-# Variables
-cards = []
-suits = ["spades", "clubs", "hearts", "diamonds"]
-ranks = [
-    {"rank": "A", "value": 11},
-    {"rank": "2", "value": 2},
-    {"rank": "3", "value": 3},
-    {"rank": "4", "value": 4},
-    {"rank": "5", "value": 5},
-    {"rank": "6", "value": 6},
-    {"rank": "7", "value": 7},
-    {"rank": "8", "value": 8},
-    {"rank": "9", "value": 9},
-    {"rank": "10", "value": 10},
-    {"rank": "J", "value": 10},
-    {"rank": "Q", "value": 10},
-    {"rank": "K", "value": 10},
-]
-# Program
-for suit in suits:
-    for rank in ranks:
-        cards.append([suit, rank])
+# Classes
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self. rank = rank
 
-def shuffle():
-    random.shuffle(cards)
+    def __str__(self): # called when printing
+        return f"{self.rank['rank']} of {self.suit}"
 
-def deal(number):
-    cards_dealt = []
-    for x in range(number):
-        card = cards.pop()
-        cards_dealt.append(card)
-    return cards_dealt
+class Deck:
+    def __init__(self):
+        self.cards = []
+        suits = ["spades", "clubs", "hearts", "diamonds"]
+        ranks = [
+            {"rank": "A", "value": 11},
+            {"rank": "2", "value": 2},
+            {"rank": "3", "value": 3},
+            {"rank": "4", "value": 4},
+            {"rank": "5", "value": 5},
+            {"rank": "6", "value": 6},
+            {"rank": "7", "value": 7},
+            {"rank": "8", "value": 8},
+            {"rank": "9", "value": 9},
+            {"rank": "10", "value": 10},
+            {"rank": "J", "value": 10},
+            {"rank": "Q", "value": 10},
+            {"rank": "K", "value": 10},
+        ]
+        
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append(Card(suit, rank))
 
-shuffle()
-card = deal(1)[0]
+    def shuffle(self):
+        if len(self.cards) > 1:
+            random.shuffle(self.cards)
 
-print(card[1]["value"])
+    def deal(self, number):
+        cards_dealt = []
+        for x in range(number):
+            if len(self.cards) > 0:
+                card = self.cards.pop()
+                cards_dealt.append(card)
+        return cards_dealt
+
+# Instances
+card1 = Card("hearts", {"rank": "J", "value": 10})
+print(card1)
